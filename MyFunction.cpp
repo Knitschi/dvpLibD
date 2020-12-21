84fb593bccff2f19e8157403fcffb1d6fb10737b
Knitschi <knitschi@freenet.de>
2020-12-21 18:10:19 +0100

    Adds initial files.

---
+++ MyFunction.cpp
@@ -0,0 +1,12 @@
+#include "dvpLibDDependencyNames.h"
+#include CPF_VERSIONED_INCLUDE(dvpLibD, MyFunction.h)
+
+CPF_VERSIONED_NAMESPACE(dvpLibD)
+{
+
+	bool isCoolMan() const
+	{
+		return true;
+	}
+
+}
---
+++ MyFunction.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "dvpLibDDependencyNames.h"
+#include CPF_VERSIONED_INCLUDE(dvpLibD, dvplibd_export.h)
+
+
+CPF_VERSIONED_NAMESPACE(dvpLibD)
+{
+	DVPLIBC_EXPORT bool isCoolMan();
+}
